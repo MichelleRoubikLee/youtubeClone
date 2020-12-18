96c43181c3073f68346f4418aea2fcb9487e7e7c
MichelleRoubikLee <michelleroubiklee@gmail.com>
2020-12-17 19:03:56 -0600

    replySchema added and linked to commentSchema

--- server/models/comments.js
+++ server/models/comments.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 
+const replySchema = new mongoose.Schema({
+    likes: {type: Number, default: 0},
+    dislikes: {type: Number, default: 0},
+    text: {type: String, required: true},
+    postDate: {type: Date, default: Date.now()}
+});
+
 const commentSchema = new mongoose.Schema({
-    videoId: {type: String},
-    likes: {type: Number},
-})
+    videoId: {type: String, required: true},
+    likes: {type: Number, default: 0},
+    dislikes: {type: Number, default: 0},
+    text: {type: String, required: true, minlength: 2, maxlength: 255 },
+    postDate: {type: Date, default: Date.now()},
+    replies: [replySchema]
+});
+
