6c14486c75b3d640b96c99980d77ae52ce8df812
MichelleRoubikLee <michelleroubiklee@gmail.com>
2020-12-17 19:48:37 -0600

    post method added to comments.js with example comment

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -2,4 +2,20 @@ const Comment = require('../models/products');
 const express = require('express');
 const router = express.Router();
 
+router.post('/', async (req,res) => {
+    try {
+        const comment = new Comment ({
+            videoId: 'H6t3wr7wARk',
+            text: 'Hello World'
+        });
+
+        await comment.save();
+
+        return res.send(product);
+
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+});
+
 module.exports = router;
