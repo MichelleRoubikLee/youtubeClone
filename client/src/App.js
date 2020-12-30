d3fdff33a77456d7841eaf4059e8d9b90a2fb7e1
MichelleRoubikLee <michelleroubiklee@gmail.com>
2020-12-29 21:14:25 -0600

    added structure for react page in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,75 @@
 import './App.css';
+import React, { useState } from 'react';
+
+//search videos with user input
+function SearchBar() {
+
+  return(
+    <div className='SearchBar'>
+
+    </div>
+  );
+}
+
+function VideoPlayer() {
+
+  return(
+    <div className='VideoPlayer'>
+
+    </div>
+  );
+}
+
+function AddComment() {
+
+  return(
+    <div className='AddComment'>
+
+    </div>
+  );
+}
+
+// function EditComment() {
+
+//   return(
+//     <div className='EditComment'>
+
+//     </div>
+//   );
+// }
+
+// function AddReply() {
+
+//   return(
+//     <div className='AddReply'>
+
+//     </div>
+//   );
+// }
+
+function ViewComments() {
+
+  return(
+    <div className='ViewComments'>
+      {/* <EditComment/>
+      <AddReply/> */}
+    </div>
+  );
+}
 
 function App() {
+  // const [comments, createComments] = useState([
+  //   {
+
+  //   }
+  // ]);
+
   return (
     <div className="App">
-      Hello World
+      <SearchBar/>
+      <VideoPlayer/>
+      <AddComment/>
+      <ViewComments/>
     </div>
   );
 }
