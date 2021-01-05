e6e5cc37d59ce5a5836b91d1120394c575541f42
MichelleRoubikLee <michelleroubiklee@gmail.com>
2021-01-05 17:28:41 -0600

    fixed merge issues

--- client/src/components/SearchBar/searchBar.jsx
+++ client/src/components/SearchBar/searchBar.jsx
@@ -24,21 +24,12 @@ class SearchBar extends Component {
   render(){
     return(
       <div>
-<<<<<<< HEAD
-        <nav className="navbar navbar-light bg-light">
-          <div className="bar container-fluid">
-            <a href="navbar" className="navbar-brand">YouTubeClone</a>
-              <form className="d-flex" onSubmit={this.handleSubmit}>
-                <input onChange={this.handleChange} value={this.state.text} className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-dark" type="submit">Search</button>
-=======
         <nav class="navbar navbar-light bg-light">
           <div class="bar container-fluid">
             <a className="navbar-brand">YouTubeClone</a>
               <form class="d-flex" onSubmit={this.handleSubmit}>
                 <input onChange={this.handleChange} value={this.state.text} className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
                 <button class="btn btn-outline-dark" type="submit">Search</button>
->>>>>>> e52fc3fff00786984b45042c0476b5e7241177d5
               </form>
           </div>
         </nav>
--- client/src/components/VideoDetails/videoDetails.jsx
+++ client/src/components/VideoDetails/videoDetails.jsx
@@ -6,19 +6,10 @@ const VideoDetails = ({video}) => {
         return (
             <div>
                 <div>
-<<<<<<< HEAD
-                    <iframe width="1168" height="657" src="https://www.youtube.com/embed/Kp3-pXoDoIw" allowFullScreen title='Video player'/>
-                </div>
-                <div>
-                    <h4 className="dev-title">DevCodeCamp shaking up traditional 4-year degree</h4>
-                    <h6>TMJ4 News</h6>
-                    <p className="border-bottom">A new Milwaukee boot camp called devCodeCamp is shaking up the traditional four-year college degree.</p>
-=======
                     <iframe width="1120" height="630" src="https://www.youtube.com/embed/Kp3-pXoDoIw" allowFullScreen title='Video player'/>
                 </div>
                 <div>
                     <h4>DevCodeCamp shaking up traditional 4-year degree</h4>
->>>>>>> e52fc3fff00786984b45042c0476b5e7241177d5
                 </div>
                 
             </div>
@@ -30,20 +21,11 @@ const VideoDetails = ({video}) => {
     return (
         <div className="container videos">
             <div>
-<<<<<<< HEAD
-                <iframe width="1168" height="657" src={videoSrc} allowFullScreen title='Video player'/>
-            </div>
-            <div>
-                <h4 className="dev-title">{video.snippet.title}</h4>
-                <p>{video.snippet.channelTitle}</p>
-                <p className="border-bottom">{video.snippet.description}</p>
-=======
                 <iframe width="1120" height="630" src={videoSrc} allowFullScreen title='Video player'/>
             </div>
             <div>
                 <h4>{video.snippet.title}</h4>
                 <p>{video.snippet.description}</p>
->>>>>>> e52fc3fff00786984b45042c0476b5e7241177d5
             </div>
         </div>
 
