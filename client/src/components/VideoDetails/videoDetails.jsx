2badbd064ad51b2d4c8a48caaf4f4786a20231d4
greedy17 <edwardroberson17@gmail.com>
2021-01-05 21:09:26 -0500

    created comment section component, added button to addcomment component.

--- client/src/components/VideoDetails/videoDetails.jsx
+++ client/src/components/VideoDetails/videoDetails.jsx
@@ -6,19 +6,12 @@ const VideoDetails = ({video}) => {
         return (
             <div>
                 <div>
-<<<<<<< HEAD
                     <iframe width="1232" height="721" src="https://www.youtube.com/embed/bSuk-H5PFXs" allowFullScreen title='Video player'/>
                 </div>
                 <div>
                     <h4 className="dev-title">Best of the Houston Rockets | 2018-19 NBA Season</h4>
                     <h6>NBA</h6>
                     <p className="border-bottom">Check out the best of the Houston Rockets from this past season!</p>
-=======
-                    <iframe width="1120" height="630" src="https://www.youtube.com/embed/Kp3-pXoDoIw" allowFullScreen title='Video player'/>
-                </div>
-                <div>
-                    <h4>DevCodeCamp shaking up traditional 4-year degree</h4>
->>>>>>> e6e5cc37d59ce5a5836b91d1120394c575541f42
                 </div>
                 
             </div>
@@ -30,20 +23,12 @@ const VideoDetails = ({video}) => {
     return (
         <div className="container videos">
             <div>
-<<<<<<< HEAD
                 <iframe width="1168" height="657" src={videoSrc} allowFullScreen title='Video player'/>
             </div>
             <div>
                 <h4 className="dev-title">{video.snippet.title}</h4>
                 <p>{video.snippet.channelTitle}</p>
                 <p className="border-bottom">{video.snippet.description}</p>
-=======
-                <iframe width="1120" height="630" src={videoSrc} allowFullScreen title='Video player'/>
-            </div>
-            <div>
-                <h4>{video.snippet.title}</h4>
-                <p>{video.snippet.description}</p>
->>>>>>> e6e5cc37d59ce5a5836b91d1120394c575541f42
             </div>
         </div>
 
