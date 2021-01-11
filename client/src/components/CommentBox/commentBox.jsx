2664aa2bef374de99311f19769588a3ec85cae90
MichelleRoubikLee <michelleroubiklee@gmail.com>
2021-01-11 10:51:05 -0600

    adding like dislike and reply to commentbox

--- client/src/components/CommentBox/commentBox.jsx
+++ client/src/components/CommentBox/commentBox.jsx
@@ -1,17 +1,68 @@
 import React from 'react';
 import './commentBox.css';
+import axios from 'axios';
 
-const CommentBox = ({text, likes}) => {
-    return(
-        <div className="comment-box">
-            <p>{text}</p>
-            <div>
-                <button className="thumbs-up"></button>{likes}
-                <button className="thumbs-down"></button>
-                <button className="reply">REPLY</button>
+class CommentBox extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            // likes: ;
+            // dislikes: ;
+            // replies: ;
+            //commentId= ;
+        };
+
+        // this.handleLike = this.handleLike.bind(this);
+        // this.handleDislike = this.handleDislike.bind(this);
+        // this.handleReply = this.handleReply.bind(this);
+    }
+
+    handleLike(event){
+        event.preventDefault();
+        
+        // axios({
+        //     method: 'put',
+        //     url: 'http://localhost:5000/api/comments/' + commentId + '/like',
+        // })
+    }
+
+    handleDisike(event){
+        event.preventDefault();
+        
+        // axios({
+        //     method: 'put',
+        //     url: 'http://localhost:5000/api/comments/' + commentId + '/dislike',
+        // })
+    }
+
+    handleReply(event){
+        event.preventDefault();
+        // axios({
+        //     method: 'put',
+        //     url: 'http://localhost:5000/api/comments/' + commentId + '/reply',
+        //     data: {
+        //         text: replyText
+        //     }
+        // })
+    }
+
+    showReplyForm(){
+
+    }
+
+    render(){
+        return(
+            <div className="comment-box">
+                <p></p>
+                <div>
+                    <button className="thumbs-up" onclick={this.handleLike}></button>
+                    <button className="thumbs-down" onclick={this.handleDislike}></button>
+                    <button className="reply" onclick={this.handleReply}>REPLY</button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    
 }
 
 export default CommentBox;
